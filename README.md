46b26f1310523c9b9667a93c1aa46fc05c2073bc
Fluzko <fluzko@gmail.com>
2020-04-04 00:36:42 -0300

    added -i -f -h

--- README.md
+++ README.md
@@ -11,9 +11,14 @@ Run the command below on your console
 ```bash
 python3 scanner.py <ip-address>
 ```
+
+for help run this command:
+```bash
+python3 scanner.py -h
+```
 eg:
 ```bash
-python3 scanner.py 192.168.0.1
+python3 scanner.py 192.168.0. -i 1 -f 65535
 ```
 
 ## Contributing
@@ -25,4 +30,5 @@ Please make sure to update tests as appropriate.
 [MIT](https://choosealicense.com/licenses/mit/)
 
 ## Changelog
+- Now you can specify the limits of the ports and added '-help' command
 - Runs on all the ports from 1-65000, takes some time...
--- scanner.py
+++ scanner.py
@@ -3,22 +3,72 @@ import sys
 import socket
 from datetime import datetime
 
-if len(sys.argv) == 2:
-    target = socket.gethostbyname(sys.argv[1])
-else:
-    print('Syntax error')
-    print('python3 scanner.py <ip>')
-    sys.exit()
 
+def helpScreen():
+    print('-' * 50)
+    print('Python port scanner')
+    print('- Basic usage')
+    print('    python3 scanner.py <ip> ')
+    print('- Optional parameters')
+    print('   -i initial port, >=1 if not provided starts at 1')
+    print('   -f final port, <=65535 if not provided ends at 65535')
+    print('-' * 50)
+
+
+initport = 1
+finalport = 65535
+
+if len(sys.argv) == 2:
+    if sys.argv[1] in ['-help', '--help', '-h', '--h']:
+        helpScreen()
+        sys.exit()
+    target = socket.gethostbyname(sys.argv[1])
+
+
+if len(sys.argv) > 2:
+    target = socket.gethostbyname(sys.argv[1])
+    if len(sys.argv) == 4 and sys.argv[2] in ['-i', '-f']:
+        if sys.argv[2] == '-i':
+            try:
+                initport = int(sys.argv[3])
+            except:
+                print('Port type error')
+                sys.exit()
+        if sys.argv[2] == '-f':
+            try:
+                finalport = int(sys.argv[3])
+            except:
+                print('Port type error')
+                sys.exit()
+    if len(sys.argv) == 6 and sys.argv[2] in ['-i', '-f'] and sys.argv[4] in ['-i', '-f']:
+        if sys.argv[2] == '-i':
+            try:
+                initport = int(sys.argv[3])
+                finalport = int(sys.argv[5])
+            except:
+                print('Port type error')
+                sys.exit()
+        else:
+            try:
+                finalport = int(sys.argv[3])
+                initport = int(sys.argv[5])
+            except:
+                print('Port type error')
+                sys.exit()
+
+if initport >= finalport:
+    print('Initial port can\'t be higher than thefinal one')
+    sys.exit()
 
 print('-' * 50)
 print('Scanning taget ' + target)
+print('from: {}'.format(initport) + ' to: {}'.format(finalport))
 initTime = datetime.now()
 print('Time started: ' + str(initTime))
 print('-' * 50)
 
 
-for port in range(1,65000):
+for port in range(initport, finalport):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(1)
@@ -37,3 +87,5 @@ for port in range(1,65000):
     except socket.error:
         print('Could not connect to server')
         sys.exit()
+
+
